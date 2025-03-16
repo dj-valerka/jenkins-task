4cce9f8fead056d808a5daaff745fee21569aa5a
dj-valerka <dj.valerka83@gmail.com>
2025-03-16 21:14:47 +0200

    !

--- vars/jfrog-cli.groovy
+++ vars/jfrog-cli.groovy
@@ -1,10 +1,10 @@
 def call () {
-                sh '''
-                    apt update && apt install -y wget;
-                    mkdir -p /usr/share/keyrings;
-                    wget -qO - https://releases.jfrog.io/artifactory/api/v2/repositories/jfrog-debs/keyPairs/primary/public | gpg --batch --yes --dearmor -o /usr/share/keyrings/jfrog.gpg
-                    echo "deb [signed-by=/usr/share/keyrings/jfrog.gpg] https://releases.jfrog.io/artifactory/jfrog-debs focal contrib" | tee /etc/apt/sources.list.d/jfrog.list
-                    apt update;
-                    apt install -y jfrog-cli-v2-jf;
-                '''
-                }
+    sh """
+        apt update && apt install -y wget;
+        mkdir -p /usr/share/keyrings;
+        wget -qO - https://releases.jfrog.io/artifactory/api/v2/repositories/jfrog-debs/keyPairs/primary/public | gpg --batch --yes --dearmor -o /usr/share/keyrings/jfrog.gpg
+        echo "deb [signed-by=/usr/share/keyrings/jfrog.gpg] https://releases.jfrog.io/artifactory/jfrog-debs focal contrib" | tee /etc/apt/sources.list.d/jfrog.list
+        apt update;
+        apt install -y jfrog-cli-v2-jf;
+    """
+}
