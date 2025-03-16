a4051fa6d968262227b65d9b6b5aa9d7f4b2ebfb
dj-valerka <dj.valerka83@gmail.com>
2025-03-16 21:56:34 +0200

    !

--- vars/publishDockerImageToJFrog.groovy
+++ vars/publishDockerImageToJFrog.groovy
@@ -2,16 +2,16 @@ def call(String credentialsId) {
     def dockerRegistryUrl = env.DOCKER_REGISTRY_URL
     def dockerRepo = env.DOCKER_REPO
     def dockerImageName = env.DOCKER_IMAGE_NAME
-    def buildNumber = env.BUILD_NUMBER  
+    def buildNumber = env.BUILD_NUMBER
     
-    // Login to JFrog Artifactory (or other Docker registry)
+    // Log in to JFrog Container Registry
     withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'JFROG_USER', passwordVariable: 'JFROG_PASSWORD')]) {
-        // Login to Docker registry using credentials
+        // Log in to JFrog Container Registry using credentials
         sh """
             echo $JFROG_PASSWORD | docker login -u $JFROG_USER --password-stdin $dockerRegistryUrl
         """
         
-        // Push the Docker image to the registry
+        // Push the Docker image to JFrog Container Registry
         sh """
             docker push ${dockerRegistryUrl}/${dockerRepo}/${dockerImageName}:${buildNumber}
         """
